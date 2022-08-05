c767325aaeabf4f941e7fdbd0045dc58066ab672
Gavin Harrold <gavinsharrold@gmail.com>
2022-08-05 00:14:00 -0400

    can search for songs and save lyrics

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from tokens import client_id, client_secret, token 
+from lyricsgenius import Genius
 
 def main():
     """Main :)"""
-    print(token)
+    genius = Genius(token)
+    #this fucking chugs sometimes for no reason
+    artist = genius.search_artist("Yung Gravy", max_songs=20)
+    artist.save_lyrics('Gravy20.json')
+
+    
 
 if __name__ == "__main__":
     main()
