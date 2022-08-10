4c6bc46f1f2e5d0d3569c40ed1c904fbeffd01fd
Gavin Harrold <gavinsharrold@gmail.com>
2022-08-10 19:43:41 -0400

    added function to calculate the average line length for tensor purposes

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import numpy as np
 
 
 
-
 def write_lyrics(artist):
+    """
+        writes lyrics with a nice break between songs to a String variable
+        @param artist: results from Genius library
+        @returns text to insert into file
+    """
     txt = ''
 
     special_chars = set([ 'ँ', 'आ', 'ए', 'क', 'ग', 'ज', 'त',
@@ -24,10 +28,27 @@ def write_lyrics(artist):
             pass
     return txt
 
+def calculate_line_average(file):
+    """
+        calculate the average line length of a given file
+        @param String file: file name to calculate average line length
+        @returns float: calculated average line length in float form
+    """
+    sum = 0
+    lineCount = 0
+    with open(file) as f:
+        for line in f:
+            lineCount+=1
+            sum += len(line)
+    return sum/lineCount
+
+
 def main():
-    """Main :)"""
+    """
+        Main :)
+    """
     genius = Genius(token)
-    #this fucking chugs for no reason
+    #this fucking chugs for no reason (very dumb)
     artist = genius.search_artist("Yung Gravy", max_songs=20)
     artist.save_lyrics('Gravy20.json')
 
@@ -36,7 +57,16 @@ def main():
     file = open("GravyLyrics200.txt", "w")
     file.write(text)
     file.close()
-
+    with open('GravyLyrics200.txt') as f:
+        text = f.read()
+    
+    vocab = sorted(set(text))
+    char_to_ind = {u:i for i, u in enumerate(vocab)}
+    ind_to_char = np.array(vocab)
+    encoded_text = np.array([char_to_ind[c] for c in text])
+    averageLineLength = calculate_line_average('GravyLyrics200.txt')
+    print(int(averageLineLength))
+    
 
 
 
