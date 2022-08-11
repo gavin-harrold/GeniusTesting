5f8b7d170e627ae851b8fc557f01c5a9e39640a8
Gavin Harrold <gavinsharrold@gmail.com>
2022-08-10 20:05:30 -0400

    sync up to match with master

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from asyncore import write
+import tensorflow as tf
 from tokens import client_id, client_secret, token 
 from lyricsgenius import Genius
 import numpy as np
-import cv2 as cv
-
 
 
 
 def write_lyrics(artist):
+    """
+        writes lyrics with a nice break between songs to a String variable
+        @param artist: results from Genius library
+        @returns text to insert into file
+    """
     txt = ''
 
     special_chars = set([ 'ँ', 'आ', 'ए', 'क', 'ग', 'ज', 'त',
        'द', 'ध', 'न', 'प', 'ब', 'म', 'य', 'र', 'व', 'श', 'ह', 'ा', 'ि',
-       'ी', 'ू', 'े', 'ै', 'ो', '्', '\u2005', '\u200c', '—', '‘', '’',
+       'ी', 'ू', 'े', 'ै', 'ो', '्', '\u2005', '\u200c', '—', 
        '\u205f', '느', '사', '어', '이', '제', '죠', '품', '회','\u0435','\xa0','\u200b'])
 
 
@@ -25,10 +28,27 @@ def write_lyrics(artist):
             pass
     return txt
 
+def calculate_line_average(file):
+    """
+        calculate the average line length of a given file
+        @param String file: file name to calculate average line length
+        @returns float: calculated average line length in float form
+    """
+    sum = 0
+    lineCount = 0
+    with open(file) as f:
+        for line in f:
+            lineCount+=1
+            sum += len(line)
+    return sum/lineCount
+
+
 def main():
-    """Main :)"""
+    """
+        Main :)
+    """
     genius = Genius(token)
-    #this fucking chugs for no reason
+    #this fucking chugs for no reason (very dumb)
     artist = genius.search_artist("Yung Gravy", max_songs=20)
     artist.save_lyrics('Gravy20.json')
 
@@ -38,6 +58,17 @@ def main():
     file.write(text)
     file.close()
 
+    #prep data for Tensor
+    with open('GravyLyrics200.txt') as f:
+        text = f.read()
+    
+    vocab = sorted(set(text))
+    char_to_ind = {u:i for i, u in enumerate(vocab)}
+    ind_to_char = np.array(vocab)
+    encoded_text = np.array([char_to_ind[c] for c in text])
+    averageLineLength = calculate_line_average('GravyLyrics200.txt')
+    print(int(averageLineLength))
+    
 
 
 
